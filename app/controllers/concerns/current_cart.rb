2922fd419a1a229ce2f47644f6359219c43e1027
 zhangshijie <cjzhang88@gmail.com>
2016-12-06 01:23:38 +0800

    cart_create

---
+++ app/controllers/concerns/current_cart.rb
@@ -0,0 +1,11 @@
+module CurrentCart
+
+    private
+
+       def set_cart
+          @cart = Cart.find(session[:card_id])
+       rescue ActiveRecord::RecordNotFound
+          @cart = Cart.create
+          session[:card_id] = @cart
+       end
+end
