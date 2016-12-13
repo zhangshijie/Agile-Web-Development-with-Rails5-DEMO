76a0193cdf9ddbd1a37ddf2569e3e10c853d5eb1
 zhangshijie <cjzhang88@gmail.com>
2016-12-13 12:15:00 +0800

    product_id -> product.id

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -3,12 +3,12 @@ class Cart < ApplicationRecord
 
 
 	def add_product(product)
-		current_item = line_items.find_by(product_id: product_id)
+		current_item = line_items.find_by(product_id: product.id)
 
 		if current_item
 			current_item.quantity += 1
 		else
-			current_item = line_items.build(product_id: product_id)
+			current_item = line_items.build(product_id: product.id)
 		end
 		current_item
 	end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161209001246) do
+ActiveRecord::Schema.define(version: 20161210163036) do
 
   create_table "carts", force: :cascade do |t|
     t.datetime "created_at", null: false
