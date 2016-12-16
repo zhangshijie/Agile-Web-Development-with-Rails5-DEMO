4a468b4ef5fc2b38fb5652fd206d1624a6555889
CJ <cjzhang88@gmail.com>
2016-12-17 02:13:59 +0800

    点击加入购物车按钮发起js请求

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -35,6 +35,7 @@ class LineItemsController < ApplicationController
       if @line_item.save
         # format.html { redirect_to @line_item.cart, notice: 'Line item was successfully created.' }
         format.html { redirect_to store_index_url }
+        format.js
         format.json { render :show, status: :created, location: @line_item }
       else
         format.html { render :new }
